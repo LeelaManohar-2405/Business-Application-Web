675f18c6076b9c16b93e6cb941270f1e9da6dc56
LeelaManohar-2405 <leelamanohar1997@gmail.com>
2021-07-24 21:33:57 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, Routes } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Business';
+export class AppComponent implements OnInit {
+  constructor(private route: Router){
+
+  }
+  ngOnInit() {  
+    this.route.navigate(['/login']);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Login/login/login.component';
+import { RegistrationComponent } from './Login/registration/registration.component';
+import { HeaderComponent } from './Common/header/header.component';
+import { FooterComponent } from './Common/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
